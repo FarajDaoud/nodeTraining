ebcd410e3eb6ed3602c4b4aad9500c80ecbe410f
Faraj Daoud <faraj.daoud@gmail.com>
2018-06-28 15:42:05 -0400

    Finished chapter 5 section 8

--- Ch05_The_File_System/S8_Writable_File_Streams/promptPerson.js
+++ Ch05_The_File_System/S8_Writable_File_Streams/promptPerson.js
@@ -2,6 +2,7 @@ var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 var fs = require("fs");
 
+
 var realPerson = {
 	name: '',
 	sayings: []
@@ -12,15 +13,27 @@ rl.question("What is the name of a real person? ", function(answer) {
 
 	realPerson.name = answer;
 
-	//
-	//	TODO: Use a Writable Stream
-	//
+	var stream = fs.createWriteStream(realPerson.name + ".md");
 
+	stream.write(`${realPerson.name}\n==============\n\n`);
 
-		//
-		//TODO: Write to the stream
-		//
-		
+	rl.setPrompt(`What would ${realPerson.name} say? `);
+
+	rl.prompt();
+
+	rl.on('line', function(saying) {
+
+		if (saying.toLowerCase().trim() === 'exit') {
+			stream.close();
+			rl.close();
+		} else {
+			realPerson.sayings.push(saying.trim());
+			stream.write(`* ${saying.trim()} \n`);
+
+			rl.setPrompt(`What else would ${realPerson.name} say? ('exit' to leave) `);
+		    rl.prompt();
+		}
+
 	});
 
 });
@@ -30,5 +43,11 @@ rl.on('close', function() {
 
 	console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
 	process.exit();
-
+	
 });
+
+
+
+
+
+	
