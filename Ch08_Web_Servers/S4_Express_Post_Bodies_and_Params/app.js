abf698b1d213089ff8263374c052f8a4e782e72c
Faraj Daoud <faraj.daoud@gmail.com>
2018-06-29 14:03:14 -0400

    Finished ch8 section 4

--- Ch08_Web_Servers/S4_Express_Post_Bodies_and_Params/app.js
+++ Ch08_Web_Servers/S4_Express_Post_Bodies_and_Params/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var cors = require("cors");
+var bodyParser = require("body-parser");
 var app = express();
 
 var skierTerms = [
@@ -17,9 +18,17 @@ var skierTerms = [
     }
 ];
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*
+So now that we've parsed this data, what happened? It just means that 
+we have gone in and parsed all the variables that are posted to this 
+application, and placed them neatly on the request object.
+*/
 
 app.use(function(req, res, next) {
-	console.log(`${req.method} request for '${req.url}'`);
+	console.log(`${req.method} request for '${req.url}' -${JSON.stringify(req.body)}`);
 	next();
 });
 
@@ -29,6 +38,23 @@ app.use(cors());
 
 app.get("/dictionary-api", function(req, res) {
 	res.json(skierTerms);
+});
+
+app.post("/dictionary-api", function(req, res) {
+    skierTerms.push(req.body);
+    res.json(skierTerms);
+});
+/*
+if you send a term to dictionary-api/ whatever term you send, I can 
+set that up as a routing variable with a colon. So this means that 
+I've just created a routing parameter called "term".
+*/
+app.delete("/dictionary-api/:term", function(req, res) {
+    skierTerms = skierTerms.filter(function(definition) {
+        return definition.term.toLowerCase() !== req.params.term.toLowerCase();
+    });
+    res.json(skierTerms);
+
 });
 
 app.listen(3000);
