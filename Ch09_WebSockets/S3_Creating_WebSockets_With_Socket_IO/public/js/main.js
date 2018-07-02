eea45b29600e170d180227769497825908885181
Faraj Daoud <faraj.daoud@gmail.com>
2018-07-02 09:01:54 -0400

    Finished ch9 section 3

--- Ch09_WebSockets/S3_Creating_WebSockets_With_Socket_IO/public/js/main.js
+++ Ch09_WebSockets/S3_Creating_WebSockets_With_Socket_IO/public/js/main.js
@@ -1,4 +1,14 @@
-
+/*
+If you create websockets with native JavaScript in the browser, they'll 
+work in most modern browsers. But the support for them in older browsers 
+isn't quite there yet. Here I'm looking up the websockets spec. on 
+caniuse.com. So, one of the things that we can do, is, we can 
+incorporate a module called Socket.IO. Socket.IO is a module that 
+will help us build websockets, that has its own client and its own 
+server JavaScript. What Socket.IO does, is it fails back to long polling 
+when websockets aren't supported. So, if websockets aren't supported in 
+a browser, Socket.IO will still most likely work.
+*/
 var socket = io("http://localhost:3000");
 
 socket.on("disconnect", function() {
