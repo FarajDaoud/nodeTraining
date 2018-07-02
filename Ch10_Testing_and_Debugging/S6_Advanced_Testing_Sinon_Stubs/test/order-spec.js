02ffdf3decb790713bc0de794bf9cc144815b8b2
Faraj Daoud <faraj.daoud@gmail.com>
2018-07-02 10:16:37 -0400

    finished ch10 section 6

--- Ch10_Testing_and_Debugging/S6_Advanced_Testing_Sinon_Stubs/test/order-spec.js
+++ Ch10_Testing_and_Debugging/S6_Advanced_Testing_Sinon_Stubs/test/order-spec.js
@@ -19,9 +19,13 @@ describe("Ordering Items", function() {
 			log: sinon.spy()
 		};
 
+		this.warehouse = {
+			packageAndShip: sinon.stub().yields(10987654321)
+		};
+
 		order.__set__("inventoryData", this.testData);
 		order.__set__("console", this.console);
-
+		order.__set__("warehouse", this.warehouse);
 	});
 
 	it("order an item when there are enough in stock", function(done) {
@@ -35,6 +39,20 @@ describe("Ordering Items", function() {
 			done();
 		});
 
+	});
+
+	describe("Warehouse interaction", function() {
+		beforeEach(function() {
+			this.callback = sinon.spy();
+			order.orderItem("CCC", 2, this.callback);
+		});
+		it("receives a tracking number", function() {
+			expect(this.callback.calledWith(10987654321)).to.equal(true);
+		});
+		
+		it("calls packageAndShip with the correct sku and quantity", function() {
+			expect.this.warehouse.packageAndShip.calledWith("CCC", 2).to.equal(true);
+		});
 	});
 
 });
