d16a36d050479a850c0f3a5bcbf13e0969d27564
Faraj Daoud <faraj.daoud@gmail.com>
2018-07-02 09:44:00 -0400

    Finished ch10 section 3

--- Ch10_Testing_and_Debugging/S3_Mocking_a_Server_With_Nock/test/tools-spec.js
+++ Ch10_Testing_and_Debugging/S3_Mocking_a_Server_With_Nock/test/tools-spec.js
@@ -1,5 +1,6 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
+var nock = require("nock");
 
 describe("Tools", function() {
 
@@ -12,12 +13,18 @@ describe("Tools", function() {
 
 	describe("loadWiki()", function() {
 
-		this.timeout(5000);
+		before(function() {
+
+			nock("https://en.wikipedia.org")
+			.get("/wiki/Abraham_Lincoln")
+			.reply(200, "Mock Abraham Lincoln Page");
+
+		});
 
 		it("Load Abraham Lincoln's wikipedia page", function(done) {
 
 			tools.loadWiki({ first: "Abraham", last: "Lincoln"}, function(html) {
-				expect(html).to.be.ok;
+				expect(html).to.equal("Mock Abraham Lincoln Page");
 				done();
 			});
 
@@ -27,4 +34,16 @@ describe("Tools", function() {
 
 });
 
+/*
+The "loadWiki" function is actually hitting Wikipedia. That's how we 
+want this function to work in production, but when we're testing it, 
+this seems a little unnecessary and a little bit time consuming. The 
+solution is to create a Mock Wikipedia, so that when we actually run 
+this test, we don't have to hit Wikipedia, but our "loadWiki" function 
+can interact with our Mock as if it were Wikipedia. There's a node 
+package called "nock" that will help us do this exact thing. We can use 
+"nock" to create mock web servers so that when we test any of our 
+functions that require hitting a web server, we can hit a fake or mock 
+web server instead.
+*/
 
